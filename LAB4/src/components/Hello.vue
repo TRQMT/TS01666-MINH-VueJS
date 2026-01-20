4c6bade87d463f976f83d7c83c2f08c2b9134a17
TRQMT <minhntts01666@gmail.com>
2026-01-20 10:41:23 +0700

    hoàn thành LAB4

---
+++ LAB4/src/components/Hello.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Chào mừng đến với Vue.js</h1>
+</template>
+
+<style>
+    h1 {
+        color: red;
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        text-align: center;
+        margin-top: 40px;
+    }
+</style>
