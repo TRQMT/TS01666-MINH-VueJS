4b0eac6d011d25ffabe5d7d7cf12822dc5e49b05
TRQMT <minhntts01666@gmail.com>
2026-02-23 10:32:13 +0700

    hoan thanh lab7

---
+++ LAB_TS01666/LAB7/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import bai1 from './components/bai1.vue';
+import bai2 from './components/bai2.vue';
+import bai3 from './components/bai3.vue';
+import bai4 from './components/bai4.vue';
+import Home from './views/Home.vue';
+import About from './views/About.vue';
+import { RouterView } from 'vue-router';
+
+</script>
+
+<template>
+  <!-- <h3>Đây là trang chủ</h3>
+  <nav>
+    
+    <RouterLink to="/">Go to home</RouterLink>
+    <br>
+    <RouterLink to="/about">Go to about</RouterLink>
+  </nav>
+  <main>
+    <RouterView/>
+  </main> -->
+
+  <!-- <bai1></bai1> -->
+  <!-- <bai2></bai2> -->
+  <!-- <bai3></bai3> -->
+  <bai4></bai4>
+</template>
+
+<style scoped>
+</style>
---
+++ LAB_TS01666/LAB7/src/views/Home.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    methods: {
+        gotoAbout() {
+            this.$router.push('/about');
+        },       
+    },
+}
+</script>
+
+<template lang="">  
+    <h2>HomeView</h2> 
+    <button @click="gotoAbout">Go to About</button>
+</template>
